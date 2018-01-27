66025a02537d0042d2be5c189cd5ce3a5895ae9f
mo0nzzz <zx0fox@gmail.com>
2018-01-27 13:51:48 -0500

    04_new_way

--- arcade_intro/04_adjacentElementsProduct.py
+++ arcade_intro/04_adjacentElementsProduct.py
@@ -1,12 +1,18 @@
-def adjacentElementsProduct(inputArray):
-    running_max = 0
-    for i in range(0, len(inputArray)-1):
-       arr = inputArray[i:i+2]
-       _max = arr[0] * arr[1]
+# First try:
+# def adjacentElementsProduct(inputArray):
+#     running_max = 0
+#     for i in range(0, len(inputArray)-1):
+#        arr = inputArray[i:i+2]
+#        _max = arr[0] * arr[1]
+#
+#        if _max > running_max or running_max == 0:
+#            running_max = _max
+#     print(running_max)
+#     return running_max
 
-       if _max > running_max or running_max == 0:
-           running_max = _max
-    print(running_max)
-    return running_max
+# One liner? ))
+def adjacentElementsProduct(inputArray):
+    x = [inputArray[i]*inputArray[i+1] for i in inputArray]
+    return max(x)
 
 adjacentElementsProduct([-23, 4, -3, 8, -12])
