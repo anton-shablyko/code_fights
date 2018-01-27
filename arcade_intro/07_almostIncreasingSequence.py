67de121903347174762912c94d1b70d063b977ff
mo0nzzz <zx0fox@gmail.com>
2018-01-27 11:15:43 -0500

    07_almost_increasing_sequence

--- arcade_intro/07_almostIncreasingSequence.py
+++ arcade_intro/07_almostIncreasingSequence.py
@@ -1,39 +1,34 @@
+def check_pairs(seq):
+    for i in range(len(seq)-1):
+        if seq[i] >= seq[i+1]:
+            return i
+    return -1
+
+
 def almostIncreasingSequence(sequence):
-    if len(sequence) <= 2:
+    j = check_pairs(sequence)
+    if j == -1:
         return True
-    # generate a list of sequences without one number
-    # [1,2,3] ==> [[2, 3], [1, 3], [2, 3]]
-    x = (sequence[:i] + sequence[i + 1:] for i in range(len(sequence)))
-    for i in x:
-        if is_increasing(i):
-            return True
-        else:
-            continue
+    if check_pairs(sequence[j-1:j] + sequence[j+1:]) == -1:
+        return True
+    if check_pairs(sequence[j:j+1] + sequence[j+2:]) == -1:
+        return True
     return False
 
-def is_increasing(seq):
-    if seq == sorted(seq):
-        if len(seq) == len(set(seq)):
-            return True
-    return False
-
-print('\nThese should be True.')
-print(almostIncreasingSequence([]))
-print(almostIncreasingSequence([1]))
-print(almostIncreasingSequence([1, 2]))
-print(almostIncreasingSequence([1, 2, 3]))
-print(almostIncreasingSequence([1, 3, 2]))
-print(almostIncreasingSequence([10, 1, 2, 3, 4, 5]))
-print(almostIncreasingSequence([0, -2, 5, 6]))
-print(almostIncreasingSequence([1, 1]))
-print(almostIncreasingSequence([1, 2, 3, 4, 3, 6]))
-print(almostIncreasingSequence([1, 2, 3, 4, 99, 5, 6]))
-print(almostIncreasingSequence([1, 2, 2, 3]))
-
-
-
-print('\nThese should be False.')
-print(almostIncreasingSequence([1, 3, 2, 1]))
-print(almostIncreasingSequence([3, 2, 1]))
-print(almostIncreasingSequence([1, 1, 1]))
-print(almostIncreasingSequence([1, 1, 1, 2, 3]))
+"""Good Solution but slow with large sets"""
+# def almostIncreasingSequence(sequence):
+#     if len(sequence) <= 2:
+#         return True
+#     x = (sequence[:i] + sequence[i + 1:] for i in range(len(sequence)))
+#     for i in x:
+#         if is_increasing(i):
+#             return True
+#         else:
+#             continue
+#     return False
+#
+# def is_increasing(seq):
+#     if seq == sorted(seq):
+#         if len(seq) == len(set(seq)):
+#             return True
+#     return False
